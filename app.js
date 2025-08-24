9b7f0c7af72af260bdb04749d31b37e8e291227d
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-08-24 10:30:27 +0500

    Mocks, Stub & Spy

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-export default function greet(name) {
-    return `Hello, ${name}`
+// DB Table
+const users = [
+    { id: 1, name: "Ali" },
+    { id: 2, name: "Bruce" },
+    { id: 3, name: "Sara" },
+];
+
+export const greet = (id, findOne) => {
+    // DB call
+    const user = findOne(id);
+    return `Hello, ${user.name}`
 }
+
+const findUserById = (userId) => users.find(user => user.id === userId);
+
+console.log(greet(2, findUserById)); // DI
--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,30 @@
-import { test } from "node:test"
-import assert from "node:assert"
-import greet from "../app.js";
+import { describe, it, mock } from "node:test";
+import assert from "node:assert";
+import { greet } from "../app.js";
 
-test('should return greet message', () => {
-    const actual = greet("Usman");
-    const expected = "Hello, Usman";
+describe('Greetings', () => {
+  it('should return greet message', () => {
+    const mockUser = { id: 101, name: "John" };
 
+    // To avoid DB call
+    const mockFindUser = mock.fn(() => mockUser);
+
+    assert.strictEqual(mockFindUser.mock.callCount(), 0);
+
+    // DI using mock function
+    const actual = greet(101, mockFindUser);
+    const expected = "Hello, John";
     assert.strictEqual(actual, expected);
-})
+
+    // Verify the mock was called once
+    assert.strictEqual(mockFindUser.mock.callCount(), 1);
+
+    const call = mockFindUser.mock.calls[0];
+
+    // Verify the mock was called with correct arguments
+    assert.deepStrictEqual(call.arguments, [101]);
+
+    // Verify the returned value from the mock call
+    assert.deepStrictEqual(call.result, mockUser);
+  });
+});
